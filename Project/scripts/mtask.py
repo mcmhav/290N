5a6df11c41f9e3b909effc277562d917a1cb4e7d
Vegar Engen <vegar.engen@gmail.com>
2013-06-02 17:46:33 -0700

    added screen option

--- Project/scripts/mtask.py
+++ Project/scripts/mtask.py
@@ -68,12 +68,14 @@ def manual_auth(username, hostname):
 
 parser = argparse.ArgumentParser(prog = 'Multiple task', description = 'Do the same task on multiple servers')
 parser.add_argument('-r', '--remove', action='store_true', help='remove file script from server after running')
+parser.add_argument('--screen', action='store_true', help='remove file script from server after running')
 parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
 parser.add_argument('-i', '--identity', nargs='?', help = 'Specify rsa key to use', dest='key_file', default=os.path.join(os.environ['HOME'], '.ssh', 'id_rsa'))
 parser.add_argument('-t', '--hosts', nargs='?', help = 'File with list of hosts', dest='hosts_file', default='hosts.txt')
 parser.add_argument('-s', '--script', nargs='?', help = 'Specifies the script to run on each host', dest='script', default='script.sh')
 parser.add_argument('-o', '--options', nargs=2, action="append", help= 'Add an option to be sent as argument for the script')
 parser.add_argument('--as-array', nargs=2, action="append", help= 'Adds an option to the script and converts a file to an array of strings' )
+
 args = parser.parse_args()
 print args
 
@@ -188,9 +190,13 @@ for host in hosts:
         print '*** Here we go!'
         print 
         chan = t.open_session()
-        command = "chmod +x %s; ./%s %s ;"
+        command = "chmod +x %s;"%args.script
+        if args.screen:
+            command += "screen -dmS %s "%args.script
+
+        command +="./%s %s ;"%(args.script, args.script, script_args)
         if args.remove:
-            command += "rm %s"%(args.script, args.script, script_args, args.script)
+            command += "rm %s"%(args.script)
         chan.exec_command(command)
         chan = t.open_session()
         chan.exec_command("echo '%s' >> log.log"%command)
