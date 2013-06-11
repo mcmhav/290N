8e0517f4adf82037be227f26a97273b02d592925
Martin Havig <mchcake@gmail.com>
2013-06-11 10:04:34 -0700

    hostnames

--- Project/scripts/start.sh
+++ Project/scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./mtask.py -s startup.sh -i ../supakey.pem -t ../conf/hosts.txt -o m ip-10-245-123-187 --as-array z ../conf/zookeepers.txt --as-array n ../conf/nodes.txt
+./mtask.py -s startup.sh -i ../supakey.pem -t ../conf/hosts.txt -o m ip-10-254-28-248 --as-array z ../conf/zookeepers.txt --as-array n ../conf/nodes.txt
